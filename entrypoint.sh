5d2e4f8fadf4fa1b5b66e8be75030f118a63896f
Leo McArdle <leomcardle@gmail.com>
2019-03-24 13:00:32 +0000

    remove SERVER_NAME

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ chown -R http:http /srv/http/pacman-cache
 
 cp nginx.conf.orig nginx.conf
 
-sed -i "s/cache\.domain\.example/$SERVER_NAME/g" nginx.conf
-
 sed -i "s/http:\/\/mirror1\.domain\.example/http:\/\/mirror\.bytemark\.co\.uk\/archlinux/g" nginx.conf
 sed -i "s/http:\/\/mirror2\.domain\.example\/archlinux/http:\/\/mirror\.bytemark\.co\.uk\/archlinux/g" nginx.conf
 sed -i "s/http:\/\/mirror3\.domain\.example\/arch/http:\/\/mirror\.bytemark\.co\.uk\/archlinux/g" nginx.conf
